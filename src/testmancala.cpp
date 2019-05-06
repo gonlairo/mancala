f0ad9bfe5e68a93e48d31d64692921e691b292ca
Haley Raizes <hrraizes@ucdavis.edu>
2019-05-06 16:58:54 -0700

    First round of tests

--- src/testmancala.cpp
+++ src/testmancala.cpp
@@ -2,15 +2,83 @@
 #include "MancalaBoard.h"
 
 TEST(MancalaBoardTest, DefaultBoardTest){
-    // Needs to test that default board is correct
+    
+    CMancalaBoard Board;
+
+    EXPECT_EQ(std::string(Board), 
+                                "P1          PITS\n"
+                                "      5   4   3   2   1\n"
+                                "/---------------------------\\\n"
+                                "|   | 4 | 4 | 4 | 4 | 4 |   |\n"
+                                "| 0 |-------------------| 0 |\n"
+                                "|   | 4 | 4 | 4 | 4 | 4 |   |\n"
+                                "\\---------------------------/\n"
+                                "      1   2   3   4   5\n"
+                                "             PITS          P2\n");
+
+    EXPECT_EQ(Board.ToString(),
+                                "P1          PITS\n"
+                                "      5   4   3   2   1\n"
+                                "/---------------------------\\\n"
+                                "|   | 4 | 4 | 4 | 4 | 4 |   |\n"
+                                "| 0 |-------------------| 0 |\n"
+                                "|   | 4 | 4 | 4 | 4 | 4 |   |\n"
+                                "\\---------------------------/\n"
+                                "      1   2   3   4   5\n"
+                                "             PITS          P2\n");
+
+    EXPECT_EQ(Board.PlayerTurn(), 1); //Player 1 goes first
+
+    EXPECT_EQ(Board.PlayerScore(1), 0); //Player 1 begins with 0 stones in their store
+    EXPECT_EQ(Board.PlayerScore(2), 0); //Player 2 begins with 0 stones in their store
+    EXPECT_EQ(Board.PlayerScore(3), -1); //PlayerScore returns -1 on bad parameter 
+    EXPECT_EQ(Board.PlayerScore(-3), -1); //PlayerScore returns -1 on bad parameter 
+
+
+    EXPECT_EQ(Board.PitStoneCount(1, 1), 4); //Player 1, Pit 1 == 4 stones
+    EXPECT_EQ(Board.PitStoneCount(1, 2), 4); //Player 1, Pit 2 == 4 stones
+    EXPECT_EQ(Board.PitStoneCount(1, 3), 4); //Player 1, Pit 3 == 4 stones
+    EXPECT_EQ(Board.PitStoneCount(1, 4), 4); //Player 1, Pit 4 == 4 stones
+    EXPECT_EQ(Board.PitStoneCount(1, 5), 4); //Player 1, Pit 5 == 4 stones
+
+    EXPECT_EQ(Board.PitStoneCount(2, 1), 4); //Player 2, Pit 1 == 4 stones
+    EXPECT_EQ(Board.PitStoneCount(2, 2), 4); //Player 2, Pit 2 == 4 stones
+    EXPECT_EQ(Board.PitStoneCount(2, 3), 4); //Player 2, Pit 3 == 4 stones
+    EXPECT_EQ(Board.PitStoneCount(2, 4), 4); //Player 2, Pit 4 == 4 stones
+    EXPECT_EQ(Board.PitStoneCount(2, 5), 4); //Player 2, Pit 5 == 4 stones
+
+    EXPECT_EQ(Board.PitStoneCount(3, 1), -1); //invalid parameter for player
+    EXPECT_EQ(Board.PitStoneCount(-3, 1), -1); //invalid parameter for player
+    EXPECT_EQ(Board.PitStoneCount(1, 6), -1); //invalid parameter for pit
+    EXPECT_EQ(Board.PitStoneCount(3, 6), -1); //invalid parameter for player and pit
+
+    EXPECT_FALSE(Board.GameOver()); //game should not be over
+
 }
 
 TEST(MancalaBoardTest, SetBoardTest){
-    // Needs to test that setting up an initial board is correct
+    CMancalaBoard DefBoard;
+    CMancalaBoard SetBoard(1, 4, 0);
+
 }
 
 TEST(MancalaBoardTest, ResetBoardTest){
-    // Needs to test that resetting a board is correct
+    CMancalaBoard ResBoard; //use move
+
+    EXPECT_EQ(ResBoard.ResetBoard().ToString(),
+                                            "P1          PITS\n"
+                                            "      5   4   3   2   1\n"
+                                            "/---------------------------\\\n"
+                                            "|   | 4 | 4 | 4 | 4 | 4 |   |\n"
+                                            "| 0 |-------------------| 0 |\n"
+                                            "|   | 4 | 4 | 4 | 4 | 4 |   |\n"
+                                            "\\---------------------------/\n"
+                                            "      1   2   3   4   5\n"
+                                            "             PITS          P2\n"); 
+
+
+    
+
 }
 
 TEST(MancalaBoardTest, BasicMoveTest){
